73ceb8fef392f252f4e86ec95f99ed32906fb60a
Niennte <niennte@gmail.com>
2018-07-13 22:11:38 -0400

    Lyrical conclusion.

--- a.rb
+++ a.rb
@@ -2,6 +2,11 @@ puts "I want to load some data from b.rb"
 
 local_dir = File.expand_path('../', __FILE__)
 $LOAD_PATH.unshift(local_dir)
+
+
+# part of bigger kernel object
+# calling a message on a object is sending a message (method) to the receiver (object)
+# - in this case, receiver is implicit (like global object in php or javascript)
 require "b.rb"
 
 puts B_DATA
