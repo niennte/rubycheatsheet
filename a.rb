8e173a259c221def2fa563bba5e85a35df09b0b2
Niennte <niennte@gmail.com>
2018-07-13 21:58:23 -0400

    Replace passing load path into the intepreter with explicit path within require call.

--- a.rb
+++ a.rb
@@ -1,9 +1,5 @@
 puts "I want to load some data from b.rb"
 
-puts "But my load path is: #{$LOAD_PATH}"
-
-puts "$ ruby -I . a.rb #will add current directory to load path"
-
-require "b.rb"
+require "./b.rb"
 
 puts B_DATA
